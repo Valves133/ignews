1a3b7e70749bbfabdf287c0e101ec02ec1825bc6
Victor <victor.h.m.alves@gmail.com>
2021-04-04 12:41:49 +0200

    Add Stripe

--- Documentos/ignite/reactjs/ignews/src/pages/index.tsx
+++ Documentos/ignite/reactjs/ignews/src/pages/index.tsx
@@ -1,14 +1,59 @@
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
+
+interface HomeProps{
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
      <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
       <h1>
-        Hello 
+        <main className={styles.contentContainer}>
+          <section className={styles.hero}>
+            <span>👏 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get acess to all the publications <br />
+              <span>for {product.amount} month</span>
+            </p>
+            <SubscribeButton priceId={product.priceId} />
+          </section>
+
+          <img src="/images/avatar.svg" alt="girl coding"/>
+        </main>
      </h1>
     </>
 )
+}
+
+export const getServerSideProps: GetServerSideProps = async() => {
+  const price = await stripe.prices.retrieve('price_1IcSK5GcWaehsmJbVZRaJsLS', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('es-Es', {
+      style: 'currency',
+      currency: 'EUR',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
